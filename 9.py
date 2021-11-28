9644d48282d73530549bc413948fd4b2a0f28181
flpcan <63620237+flpcan@users.noreply.github.com>
2021-11-28 15:47:46 -0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -6,20 +6,22 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-
-
 lst = list(range(0,1000))
+numbers = []
+finals= []
 i=int()
 j= int()
 k= int()
 for i in lst:
-    for j in lst:
-        for k in lst:
-            if i + j+k ==1000 and i<j<k == True:
-                print(i,j,k)
-                if pow(i,2) + pow(b,2) == pow(c,2):
-                    print(i,j,k)
-                    
-                    
-                    
-# Testing
+    for j in range(0,1000):
+        for k in range(0,1000):
+            if i+j+k == 1000:
+                numbers.append([i,j,k])
+for i in numbers:
+    if i[0] < i[1] and i[1] < i[2]:
+        finals.append(i)
+print(finals)
+for i in finals:
+    last = pow(i[0],2) + pow(i[1],2)
+    if last == pow(i[2],2):
+        print("Final :",i)
